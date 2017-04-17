f0cd945c48201ecbc2f8234f6eb177f53b7a9a89
Jon <jonegeland@gmail.com>
2017-04-17 16:58:42 -0400

    Unsplitting and Timer resumes. Pressing `m` will now decrement segments to the previous one and reset progress on the previously-active segment. This also works after the Run has finished, and will resume timing where it would have been had the split not occurred.

--- app/src/core/run.js
+++ app/src/core/run.js
@@ -9,7 +9,7 @@ class Run {
   };
 
   initialize() {
-    this.active_segment = 0;
+    this.segment_idx = 0;
     this.state = Run.STATES.not_started;
     for(let segment of this.segments) {
       segment.reset();
@@ -20,19 +20,39 @@ class Run {
     this.segments.push(new Segment(this, name));
   };
 
+  current_segment() {
+    return this.segments[this.segment_idx];
+  };
+
 
   split() {
-    if(this.active_segment < this.segments.length - 1) {
-      this.active_segment += 1;
+    if(this.timer.state == Run.STATES.not_started) return;
+    if(this.segment_idx < this.segments.length - 1) {
+      this.current_segment().leave(this.timer.elapsed_time);
+      this.segment_idx += 1;
+      this.current_segment().enter(this.timer.elapsed_time);
     } else {
       this.finish();
+    }
+  };
+
+  unsplit() {
+    if(this.timer.state == Run.STATES.not_started) return;
+    if(this.state == Run.STATES.finished) {
+      this.resume();
+    } else if(this.segment_idx > 0) {
+      let curr_seg = this.current_segment();
+      this.segment_idx -= 1;
+      let prev_seg = this.current_segment();
+      prev_seg.add_time(curr_seg.elapsed_time);
+      curr_seg.reset();
     }
   };
 
 
   update(timer_data) {
     if(this.state == Run.STATES.in_progess) {
-      this.segments[this.active_segment].add_time(timer_data.delta);
+      this.current_segment().add_time(timer_data.delta);
     }
   };
 
@@ -40,6 +60,7 @@ class Run {
   start() {
     this.state = Run.STATES.in_progess;
     this.timer.start();
+    this.current_segment().enter(0);
   };
 
   stop() {
@@ -53,17 +74,17 @@ class Run {
   resume() {
     this.state = Run.STATES.in_progess;
     this.timer.resume();
-  }
+  };
 
   reset() {
     this.timer.reset();
     this.initialize();
-  }
+  };
 
   finish() {
     this.stop();
     this.state = Run.STATES.finished;
-  }
+  };
 
 
   start_or_resume() {
@@ -75,8 +96,8 @@ class Run {
   };
 };
 
-Run.STATES = {
+Run.STATES = Object.freeze({
   not_started: 'not started',
   in_progess: 'in progess',
   finished: 'finished'
-}
+});
--- app/src/core/segment.js
+++ app/src/core/segment.js
@@ -1,8 +1,13 @@
 class Segment {
-  constructor(run, name, initial_time) {
-    this.run          = run;
-    this.name         = name || 'Segment';
-    this.elapsed_time = initial_time || 0;
+  constructor(run, name) {
+    this.run            = run;
+    this.pb_time        = null;
+    this.best_time      = null;
+    this.old_best_time  = null;
+    this.name           = name || 'Segment';
+    this.elapsed_time   = 0;
+    this.started_at     = null;
+    this.ended_at       = null;
   };
 
   add_time(millis) {
@@ -10,6 +15,24 @@ class Segment {
   };
 
   reset() {
+    this.best_time = this.old_best_time;
     this.elapsed_time = 0;
+    this.started_at = 0;
   };
-}
+
+  enter(starting_time) {
+    this.started_at = starting_time;
+  };
+
+  leave(ending_time) {
+    this.ended_at = ending_time;
+    if(!this.best_time || this.elapsed_time < this.best_time) {
+      this.old_best_time = this.best_time;
+      this.best_time = this.elapsed_time;
+    }
+  };
+
+  time_in_run() {
+    return this.started_at + this.elapsed_time;
+  };
+};
